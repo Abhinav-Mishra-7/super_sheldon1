92c1589a097532f4e2f3f19556fd581ea88b0421
sandstorm1243 <66744916+sandstorm1243@users.noreply.github.com>
2025-09-04 22:09:40 +0530

    review section made

---
+++ src/app/testimonial/page.js
@@ -0,0 +1,14 @@
+import ReviewsSection from "@/components/testimonial/ReviewsSections";
+import Header from "@/components/HeroHeaderNav";
+import Footer from "@/components/Footer";
+export default function Home() {
+  return (
+    <>
+    <Header/>
+      <ReviewsSection />
+    <Footer/> 
+
+      
+    </>
+  );
+}
